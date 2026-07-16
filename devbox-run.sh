e2bb3dae8cba93757dd6252097be3e41e29d1a40
Huned Botee <24231+huned@users.noreply.github.com>
2026-07-16 12:10:45 -0500

    Container inherits host's TERM env var.

--- devbox-run.sh
+++ devbox-run.sh
@@ -4,6 +4,7 @@ podman run \
   --network host \
   --hostname devbox.local \
   --name devbox \
+  --env TERM="$TERM" \
   --userns keep-id \
   --volume ~/.ssh:/home/ubuntu/.ssh:ro \
   --volume ~/work:/home/ubuntu/work:ro \
